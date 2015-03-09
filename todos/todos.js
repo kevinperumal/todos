af2fd1a6a44aaf3c875fbd552862a9246f9dec5e
Kevin Perumal <kevin.perumal@gmail.com>
2015-03-09 16:13:14 -0400

    add bootstrap, implement check box for todos

--- todos/todos.js
+++ todos/todos.js
@@ -6,6 +6,24 @@ if (Meteor.isClient) {
       return Todos.find();
     }
   });
+
+  Template.TodoItem.helpers({
+    isDoneChecked: function() {
+      return this.is_done ? 'checked' : "";
+    }
+  });
+
+  Template.TodoItem.events({
+    'click [name=is_done]': function(e, tmpl) {
+      var id = this._id;
+      var isDone = tmpl.find('input').checked;
+      Todos.update({_id: id}, {
+        $set: {
+          is_done: isDone
+        }
+      });
+    }
+  });
 }
 
 if (Meteor.isServer) {
